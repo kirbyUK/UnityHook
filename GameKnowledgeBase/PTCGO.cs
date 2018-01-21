f99fea5a0318a2d24b5ca84e2f9862cc20ab715e
Alex Kerr <kirby@cpan.org>
2018-01-21 21:00:56 +0000

    Print names of played Pokemon to a file

---
+++ GameKnowledgeBase/PTCGO.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameKnowledgeBase
+{
+	class PTCGO : IKnowledge
+	{
+		public const string WIN_REL_LIBRARY_PATH = "Pokemon Trading Card Game Online_Data\\Managed";
+
+		// File names of all assemblies, with dll extension.
+		private static string[] _assemblyFileNames = new string[]
+		{
+			"", // Empty/Default entry.
+			"core.dll",
+			"pie-core.dll",
+			"pie-src.dll",
+			"sausage-core.dll",
+			"System.dll",
+			"UnityEngine.dll",
+		};
+
+		public string[] LibraryFileNames
+		{
+			get
+			{
+				return _assemblyFileNames;
+			}
+		}
+
+		public string LibraryRelativePath
+		{
+			get
+			{
+				return WIN_REL_LIBRARY_PATH;
+			}
+		}
+	}
+}
